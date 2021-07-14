f89ca864d9bf1c2f2897bf76889fff59c7a4d9c8
Andreas D Abaidoo <abaidooandy@gmail.com>
2021-07-14 18:02:11 +0000

    Funtion to play one game round

--- game.js
+++ game.js
@@ -1,31 +1,65 @@
 console.log('Game Started: Rock Paper Scissors!');
 
-let computerSelection = Math.floor(Math.random() * 3) + 1;
-
 function computerPlay() {
+	// Create a random selection for the computer 1 - 3
+	let computerSelection = Math.floor(Math.random() * 3) + 1;
+
+	// If 1, Rock. 2, Paper. 3, Scissors
 	switch (computerSelection) {
 		case 1:
-			option = 'Rock';
-			console.log(option);
+			return 'Rock';
 			break;
 		case 2:
-			option = 'Paper';
-			console.log(option);
+			return 'Paper';
 			break;
 		case 3:
-			option = 'Scissors';
-			console.log(option);
+			return 'Scissors';
 		default:
 			break;
 	}
 }
+//  assign choice of computer
+let computerChoice = computerPlay();
 
-computerPlay();
+// Take input from user
+let playerSelection = prompt('Enter rock, paper or scissors');
 
-// let playerSelection = prompt('Enter rock, paper or scissors');
+function playRound(userChoice, compChoice) {
+	//Check for draw / same selection
+	if (userChoice.toLowerCase() === compChoice.toLowerCase()) {
+		console.log(`You draw, ${userChoice.toLowerCase()} ties with ${compChoice.toLowerCase()}`);
+	} else if (playerSelection.toLowerCase() !== compChoice.toLowerCase()) {
+		// Check for different selection
+		// rock and paper
+		if (playerSelection.toLowerCase() == 'rock' && compChoice.toLowerCase() == 'paper') {
+			console.log(`You lose, ${userChoice.toLowerCase()} loses to ${compChoice.toLowerCase()}`);
+		}
+		// rock and scissors
+		if (playerSelection.toLowerCase() == 'rock' && compChoice.toLowerCase() == 'scissors') {
+			console.log(`You win, ${userChoice.toLowerCase()} beats ${compChoice.toLowerCase()}`);
+		}
 
-// function round(playerSelection, computerSelection) {
-//     if (playerSelection.toLowerCase() == ) {
+		// paper and scissors
+		if (playerSelection.toLowerCase() == 'paper' && compChoice.toLowerCase() == 'scissors') {
+			console.log(`You lose, ${userChoice.toLowerCase()} loses to ${compChoice.toLowerCase()}`);
+		}
 
-//     }
-// }
+		// paper and rock
+		if (playerSelection.toLowerCase() == 'paper' && compChoice.toLowerCase() == 'rock') {
+			console.log(`You win, ${userChoice.toLowerCase()} beats ${compChoice.toLowerCase()}`);
+		}
+
+		// scissors and rock
+		if (playerSelection.toLowerCase() == 'scissors' && compChoice.toLowerCase() == 'rock') {
+			console.log(`You lose, ${userChoice.toLowerCase()} loses to ${compChoice.toLowerCase()}`);
+		}
+
+		// scissors and paper
+		if (playerSelection.toLowerCase() == 'scissors' && compChoice.toLowerCase() == 'paper') {
+			console.log(`You win, ${userChoice.toLowerCase()} beats ${compChoice.toLowerCase()}`);
+		}
+	}
+}
+
+// Call the function to play a round
+playRound(playerSelection, computerChoice);
