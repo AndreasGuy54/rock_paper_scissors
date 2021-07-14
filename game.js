44c033c0a9f14ce9223077a5503d7c8213ffb067
Andreas D Abaidoo <abaidooandy@gmail.com>
2021-07-14 13:24:44 +0000

    Funtion to generate random computer option

--- game.js
+++ game.js
@@ -1,1 +1,31 @@
-console.log('Game Started');
+console.log('Game Started: Rock Paper Scissors!');
+
+let computerSelection = Math.floor(Math.random() * 3) + 1;
+
+function computerPlay() {
+	switch (computerSelection) {
+		case 1:
+			option = 'Rock';
+			console.log(option);
+			break;
+		case 2:
+			option = 'Paper';
+			console.log(option);
+			break;
+		case 3:
+			option = 'Scissors';
+			console.log(option);
+		default:
+			break;
+	}
+}
+
+computerPlay();
+
+// let playerSelection = prompt('Enter rock, paper or scissors');
+
+// function round(playerSelection, computerSelection) {
+//     if (playerSelection.toLowerCase() == ) {
+
+//     }
+// }
